47dc173fcf8b023bc1519174ee5005de02ccb255
moong414 <moongom414@gmail.com>
2025-06-17 17:53:26 +0900

    리스트 페이지 추가

---
+++ lib/Page02.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Page02 extends StatelessWidget {
+  const Page02({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: const Center(child: Text('ddd')),
+    );
+  }
+}
---
+++ lib/Page03.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Page03 extends StatelessWidget {
+  const Page03({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: const Center(child: Text('ddd')),
+    );
+  }
+}
